bbd12d51cf2d3696194e6062473b42fb0291675f
bnu <bnufactory@gmail.com>
2016-04-14 14:48:29 +0900

    패널 디자인 적용

--- views/menuType/create.blade.php
+++ views/menuType/create.blade.php
@@ -1,53 +1,49 @@
 <div class="panel-group" id="accordion">
-    <div class="panel panel-default" id="panel2">
+    <div class="panel" id="panel2">
         <div class="panel-heading">
-            <h4 class="panel-title">
-                <a data-toggle="collapse" data-target="#collapseMenuType"
-                   href="#collapseMenuType">
+            <div class="pull-left">
+                <h4 class="panel-title">
                     게시판 기본 설정
-                </a>
-            </h4>
-
+                </h4>
+            </div>
+            <div class="pull-right">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseMenuType" class="btn-link panel-toggle pull-right"><i class="xi-angle-down"></i><i class="xi-angle-up"></i><span class="sr-only">메뉴닫기</span></a>
+            </div>
         </div>
-        <div id="collapseMenuType" class="panel-collapse collapse">
+        <div id="collapseMenuType" class="panel-collapse">
             <div class="panel-body">
+                <div class="form-group">
+                    <label>Board Name</label>
+                    <input type="text" name="boardName" class="form-control" value="{{Input::old('boardName', $config->get('boardName'))}}"/>
+                </div>
+                <div class="form-group">
+                    <label>Skin</label>
+                    <select name="skinId" class="form-control">
+                        @foreach($skins as $key=>$skin)
+                        <option value="{{$skin->getId()}}">{{$skin->getTitle()}}</option>
+                        @endforeach
+                    </select>
 
-                <dl>
-                    <dt>Board Name</dt>
-                    <dd><input type="text" name="boardName" class="form-control" value="{{Input::old('boardName', $config->get('boardName'))}}"/></dd>
-                    <dt>Skin</dt>
-                    <dd>
-                        <select name="skinId" class="form-control">
-                            @foreach($skins as $key=>$skin)
-                                <option value="{{$skin->getId()}}">{{$skin->getTitle()}}</option>
-                            @endforeach
-                        </select>
-                    </dd>
-                </dl>
+                </div>
 
-                <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span> For web master's
-                <p>이 설정은 등록 할 때만 처리 됩니다. 설정 변경 시 이 항목은 제공되지 않습니다.</p>
-                <div style="padding:10px; border-top:1px solid #ccc;">
+                <div class="form-group">
 
-                    <dl>
-                        <dt>Table Division {{ $config->get('boardId') }}</dt>
-                        <dd>
-                            <select name="division" class="form-control">
-                                <option value="true" {{($config->get('division') == true) ? 'selected="selected"' : ''}}>Use</option>
-                                <option value="false" {{($config->get('division') == false) ? 'selected="selected"' : ''}}>Disuse</option>
-                            </select>
-                        </dd>
-                    </dl>
+                    <p><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span> For web master's<br>이 설정은 등록 할 때만 처리 됩니다. 설정 변경 시 이 항목은 제공되지 않습니다.</p>
 
-                    <dl>
-                        <dt>Revision</dt>
-                        <dd>
-                            <select name="revision" class="form-control">
-                                <option value="true" {{($config->get('revision') == true) ? 'selected="selected"' : ''}}>Use</option>
-                                <option value="false" {{($config->get('revision') == false) ? 'selected="selected"' : ''}}>Disuse</option>
-                            </select>
-                        </dd>
-                    </dl>
+                    <label>Table Division {{ $config->get('boardId') }}</label>
+                    <select name="division" class="form-control">
+                        <option value="true" {{($config->get('division') == true) ? 'selected="selected"' : ''}}>Use</option>
+                        <option value="false" {{($config->get('division') == false) ? 'selected="selected"' : ''}}>Disuse</option>
+                    </select>
+
+                </div>
+                <div class="form-group">
+
+                    <label>Revision</label>
+                    <select name="revision" class="form-control">
+                        <option value="true" {{($config->get('revision') == true) ? 'selected="selected"' : ''}}>Use</option>
+                        <option value="false" {{($config->get('revision') == false) ? 'selected="selected"' : ''}}>Disuse</option>
+                    </select>
                 </div>
 
             </div>
