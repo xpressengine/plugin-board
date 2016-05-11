5f28771b2c61e8da48efe2cd70b8387583ec4fe2
akasima <akasima@nate.com>
2016-05-11 12:11:11 +0900

    Revert "#17 Core에서 스킨 설정을 지원함"
    This reverts commit 214ece0744aa15ab7438cfd821678b4cbc78415a.

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -131,6 +131,8 @@ class UserController extends Controller
             };
         }
 
+        // set Skin
+        XePresenter::setSkinTargetId(BoardModule::getId());
         XePresenter::share('handler', $handler);
         XePresenter::share('configHandler', $configHandler);
         XePresenter::share('urlHandler', $urlHandler);
