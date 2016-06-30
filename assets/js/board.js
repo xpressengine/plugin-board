d99d1f23dfa230217ed77aa06ca90b11209d9f2d
seungmanchoi <blueng.choi@gmail.com>
2016-06-30 14:46:27 +0900

    XE.toast type변경

--- assets/js/board.js
+++ assets/js/board.js
@@ -431,7 +431,7 @@ $(function($) {
             instanceId = $('.__xe_copy').find('[name="copyTo"]').val();
 
         if (instanceId == '') {
-            XE.toast('xe-warning', XE.Lang.trans('board::selectBoard'));
+            XE.toast('warning', XE.Lang.trans('board::selectBoard'));
             return;
         }
 
@@ -457,7 +457,7 @@ $(function($) {
             instanceId = $('.__xe_move').find('[name="moveTo"]').val();
 
         if (instanceId == '') {
-            XE.toast('xe-warning', XE.Lang.trans('board::selectBoard'));
+            XE.toast('warning', XE.Lang.trans('board::selectBoard'));
             return;
         }
 
@@ -511,7 +511,7 @@ $(function($) {
 
     var hasChecked = function() {
         if ($('.bd_manage_check:checked').length == 0) {
-            XE.toast('xe-warning', XE.Lang.trans('board::selectPost'));
+            XE.toast('warning', XE.Lang.trans('board::selectPost'));
             return false;
         }
         return true;
