83806115429c48b966bb1c145a0d8d81a6aa0b4b
akasima-mac18 <osh@xef.kr>
2018-07-09 17:30:31 +0900

    #194 notification notice 사용해 이메일 전송하도록 개선

--- src/helpers.php
+++ src/helpers.php
@@ -13,6 +13,8 @@
  */
 use Xpressengine\Plugins\Board\Models\Board;
 use Xpressengine\Menu\Models\MenuItem;
+use Xpressengine\Support\Notifications\Notice;
+use Illuminate\Notifications\Notifiable;
 
 if (function_exists('get_board_title') === false) {
     /**
@@ -47,5 +49,54 @@ if (function_exists('get_board_title') === false) {
         }
 
         return null;
+    }
+}
+
+if (function_exists('send_notice_email') === false) {
+    function send_notice_email($email, $title, $contents, callable $subjectResolver = null)
+    {
+        (new class($email, $title, $contents, $subjectResolver) {
+            use Notifiable;
+
+            protected $email;
+            protected $title;
+            protected $contents;
+            protected $subjectResolver;
+
+            public function __construct($email, $title, $contents, callable $subjectResolver = null)
+            {
+                $this->email = $email;
+                $this->title = $title;
+                $this->contents = $contents;
+                $this->subjectResolver = $subjectResolver;
+            }
+
+            /**
+             * Invoke the instance
+             *
+             * @return void
+             */
+            public function __invoke()
+            {
+                if ($this->subjectResolver != null) {
+                    Notice::setSubjectResolver($this->subjectResolver);
+                }
+
+                $this->notify(new Notice($this->email, $this->title, $this->contents));
+
+                Notice::setSubjectResolverToNull();
+            }
+
+            /**
+             * Get the notification routing information for the given driver.
+             *
+             * @param string $driver driver
+             * @return mixed
+             */
+            public function routeNotificationFor($driver)
+            {
+                return $this->email;
+            }
+        })();
     }
 }
