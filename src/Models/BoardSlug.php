030e1f47c8198d0b19090386418da88825bf30ae
sirwoongke <fzwoongbin@naver.com>
2020-01-03 15:49:14 +0900

    #258 PHP 7.4에서 정상적으로 동작하지 않는 문제 수정

--- src/Models/BoardSlug.php
+++ src/Models/BoardSlug.php
@@ -111,7 +111,7 @@ class BoardSlug extends DynamicModel
         if ($len <= 0) {
             return false;
         }
-        $h = ord($ch{0});
+        $h = ord($ch[0]);
         if ($h <= 0x7F) {
             return $h;
         }
@@ -119,13 +119,13 @@ class BoardSlug extends DynamicModel
             return false;
         }
         if ($h <= 0xDF && $len>1) {
-            return ($h & 0x1F) <<  6 | (ord($ch{1}) & 0x3F);
+            return ($h & 0x1F) <<  6 | (ord($ch[1]) & 0x3F);
         }
         if ($h <= 0xEF && $len>2) {
-            return ($h & 0x0F) << 12 | (ord($ch{1}) & 0x3F) << 6 | (ord($ch{2}) & 0x3F);
+            return ($h & 0x0F) << 12 | (ord($ch[1]) & 0x3F) << 6 | (ord($ch[2]) & 0x3F);
         }
         if ($h <= 0xF4 && $len>3) {
-            return ($h & 0x0F) << 18 | (ord($ch{1}) & 0x3F) << 12 | (ord($ch{2}) & 0x3F) << 6 | (ord($ch{3}) & 0x3F);
+            return ($h & 0x0F) << 18 | (ord($ch[1]) & 0x3F) << 12 | (ord($ch[2]) & 0x3F) << 6 | (ord($ch[3]) & 0x3F);
         }
         return false;
     }
