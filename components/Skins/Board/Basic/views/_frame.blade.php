cc3a1160f45c0405bf842734815f14543a167dc5
bnu <bnufactory@gmail.com>
2020-04-16 16:56:49 +0900

    Revert "basic 스킨 assets 추가"
    This reverts commit d725c8f7f691ea972215176d5e44cb793a223c0c.

--- components/Skins/Board/Basic/views/_frame.blade.php
+++ components/Skins/Board/Basic/views/_frame.blade.php
@@ -1,5 +1,5 @@
-{{ XeFrontend::js('plugins/board/assets/basic/js/board.js')->appendTo('body')->load() }}
-{{ XeFrontend::css('plugins/board/assets/basic/css/board.css')->load() }}
+{{ XeFrontend::js('plugins/board/assets/js/board.js')->appendTo('body')->load() }}
+{{ XeFrontend::css('plugins/board/assets/css/board.css')->load() }}
 
 <!-- BOARD -->
 <div class="board">
