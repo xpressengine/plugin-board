858175c5f26ebccfc53e88cd5b7b1b30ebd0b969
sirwoongke <fzwoongbin@naver.com>
2020-05-07 23:19:54 +0900

    클래스 오타 수정

--- components/Skins/Board/NewBlog/views/index.blade.php
+++ components/Skins/Board/NewBlog/views/index.blade.php
@@ -256,7 +256,7 @@
 
                                 @if (in_array('dissent_count', $skinConfig['listColumns']) === true)
                                     <p class="xe-list-blog-board-list-item___detail xe-list-blog-board-list-item___detail-vote_count">
-                                        <span class="xe-list-blog-board-leist-item___detail-label">{{ xe_trans('board::dissent_count') }}</span> <span class="xe-list-board-list-item___detail-number">{{ number_format($item->dissent_count) }}</span>
+                                        <span class="xe-list-blog-board-list-item___detail-label">{{ xe_trans('board::dissent_count') }}</span> <span class="xe-list-board-list-item___detail-number">{{ number_format($item->dissent_count) }}</span>
                                     </p>
                                 @endif
                             </div>
