c414913dd07c8d7637c8c89db988453aa29c3eb0
sirwoongke <fzwoongbin@naver.com>
2020-05-07 21:57:00 +0900

    신규 스킨 페이지네이션 완성

--- components/Skins/Board/NewCommon/views/default-pagination.blade.php
+++ components/Skins/Board/NewCommon/views/default-pagination.blade.php
@@ -2,22 +2,44 @@
 
 <div class="xe-list-board--pagination xe-list-board--pagination-pc">
     <ul class="xe-list-board--pagination-list">
-        <li class="xe-list-board__pagination-item xe-list-board__btn_pagination xe-list-board__btn_prev"><a class="xe-list-board__pagination-item-link" href="#"><i class="xi-angle-left"></i></a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number xe-list-board__pagination-number--active"><a class="xe-list-board__pagination-item-link" href="#">1</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">2</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">3</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">4</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">5</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">6</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">7</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">8</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">9</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__pagination-number"><a class="xe-list-board__pagination-item-link" href="#">10</a></li>
-        <li class="xe-list-board__pagination-item xe-list-board__btn_pagination xe-list-board__btn_next"><a class="xe-list-board__pagination-item-link" href="#"><i class="xi-angle-right"></i></a></li>
+        <li class="xe-list-board__pagination-item xe-list-board__btn_pagination xe-list-board__btn_prev">
+            @if ($paginator->currentPage() <= 1)
+                <a class="xe-list-board__pagination-item-link xe-list-board__pagination-item-disabled-link" onclick="return false;"><i class="xi-angle-left"></i></a>
+            @else
+                <a class="xe-list-board__pagination-item-link" href="{{ $paginator->previousPageUrl() }}"><i class="xi-angle-left"></i></a>
+            @endif
+        </li>
+        
+        @foreach ($elements as $element)
+            @if (is_array($element) === true)
+                @foreach ($element as $page => $url)
+                    @if ($page === $paginator->currentPage())
+                        <li class="xe-list-board__pagination-item xe-list-board__pagination-number xe-list-board__pagination-number--active">
+                            <a class="xe-list-board__pagination-item-link" onclick="return false;">{{ $page }}</a>
+                        </li>
+                    @else
+                        <li class="xe-list-board__pagination-item xe-list-board__pagination-number">
+                            <a class="xe-list-board__pagination-item-link" href="{{ htmlentities($url) }}">{{ $page }}</a>
+                        </li>
+                    @endif
+                @endforeach
+            @elseif (is_string($element) === true)
+                <li class="xe-list-board__pagination-item xe-list-board__pagination-number">
+                    <a class="xe-list-board__pagination-item-link" onclick="return false;">{{ $element }}</a>
+                </li>
+            @endif
+        @endforeach
+        
+        <li class="xe-list-board__pagination-item xe-list-board__btn_pagination xe-list-board__btn_next">
+            @if ($paginator->hasMorePages() === true)
+                <a class="xe-list-board__pagination-item-link" href="{{ $paginator->nextPageUrl() }}"><i class="xi-angle-right"></i></a>
+            @else
+                <a class="xe-list-board__pagination-item-link xe-list-board__pagination-item-disabled-link" onclick="return false;"><i class="xi-angle-right"></i></a>
+            @endif
+        </li>
     </ul>
 </div>
 
-<!-- 모바일 -->
 <div class="xe-list-board--pagination xe-list-board--pagination-mobile">
     <ul class="xe-list-board--pagination-list">
         <li class="xe-list-board__pagination-item xe-list-board__btn_pagination xe-list-board__btn_prev">
@@ -28,10 +50,14 @@
             @endif
         </li>
         <li class="xe-list-board__pagination-item xe-list-board__pagination-box">
-            <span class="xe-list-board__pagination-number-present">1</span> / <span class="xe-list-board__pagination-number-total">10</span>
+            <span class="xe-list-board__pagination-number-present">{{ $paginator->currentPage() }}</span> / <span class="xe-list-board__pagination-number-total">{{ $paginator->lastPage() }}</span>
         </li>
         <li class="xe-list-board__pagination-item xe-list-board__btn_pagination xe-list-board__btn_next">
-            <a class="xe-list-board__pagination-item-link" href="#"><i class="xi-angle-right"></i></a>
+            @if ($paginator->hasMorePages() === true)
+                <a class="xe-list-board__pagination-item-link" href="{{ $paginator->nextPageUrl() }}"><i class="xi-angle-right"></i></a>
+            @else
+                <a class="xe-list-board__pagination-item-link xe-list-board__pagination-item-disabled-link" onclick="return false;"><i class="xi-angle-right"></i></a>
+            @endif
         </li>
     </ul>
 </div>
