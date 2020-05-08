2b1beeb36881df101abca095b0af1b33783a93c3
mjae404 <mjae404@gmail.com>
2020-05-08 11:35:07 +0900

    new board skin 비회원 작성글 수정시 나오는 페이지 마크업 클래스 및 스타일 수정

--- components/Skins/Board/NewCommon/views/guestId.blade.php
+++ components/Skins/Board/NewCommon/views/guestId.blade.php
@@ -1,25 +1,25 @@
 {{ XeFrontend::rule('board', $rules) }}
 
 {!! XeFrontend::css('assets/core/user/auth.css')->load() !!}
-<div class="user">
-    <h1>{{ xe_trans('xe::identification') }}</h1>
+<div class="user xe-board-body">
+    <h1 class="xe-board-body__title">{{ xe_trans('xe::identification') }}</h1>
     <form action="{{ $urlHandler->get('guest.certify', ['id' => $item->id]) }}" method="post" data-rule="board">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="hidden" name="referrer" value="{{$referrer}}" />
-        <input type="hidden" name="id" value="{{$item->id}}" />
+        <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+        <input type="hidden" name="referrer" value="{{$referrer}}"/>
+        <input type="hidden" name="id" value="{{$item->id}}"  />
 
         <fieldset>
             <legend>{{ xe_trans('xe::identification') }}</legend>
-            <div class="auth-group">
+            <div class="auth-group xe-board-body__input">
                 <label for="name" class="sr-only">{{ xe_trans('xe::email') }}</label>
-                <input name="email" type="email" class="xe-form-control" value="{{ old('email') }}" placeholder="{{ xe_trans('xe::email') }}">
+                <input name="email" type="email" class="xe-form-control xe-board-body__input-email" value="{{ old('email') }}" placeholder="{{ xe_trans('xe::email') }}">
             </div>
-            <div class="auth-group">
+            <div class="auth-group xe-board-body__input">
                 <label for="pwd" class="sr-only">{{ xe_trans('xe::password') }}</label>
-                <input name="certify_key" type="password" class="xe-form-control" placeholder="{{ xe_trans('xe::password') }}">
+                <input name="certify_key" type="password" class="xe-form-control xe-board-body__input-password" placeholder="{{ xe_trans('xe::password') }}">
             </div>
 
-            <button type="submit" class="xe-btn xe-btn-primary">{{ xe_trans('xe::confirm') }}</button>
+            <button type="submit" class="xe-list-board__btn">{{ xe_trans('xe::confirm') }}</button>
         </fieldset>
     </form>
 </div>
