85d0e5b1c083e50cb0779d50e9280fbedf9c3868
sirwoongke <fzwoongbin@naver.com>
2020-05-13 20:14:56 +0900

    신규 스킨 게시판 제목 목록 페이지에서만 보이도록 수정

--- components/Skins/Board/XEDefault/views/_frame.blade.php
+++ components/Skins/Board/XEDefault/views/_frame.blade.php
@@ -12,12 +12,12 @@
 
 <section class="xe-list-board container">
     <div class="xe-list-board-header">
-        @if (request()->segment(2) !== 'print')
+        @if (request()->segment(2) === null)
             <div class="xe-list-board-header__title-content">
                 <div class="xe-list-board-header__title-box">
                     @if (in_array(array_get($skinConfig, 'titleStyle', 'titleWithCount'), ['titleWithCount', 'title']) === true)
                         <h2 class="xe-list-board-header__title"><a href="{{ $urlHandler->get('index') }}">{{ xe_trans(current_menu()['title']) }}</a></h2>
-                        @if (request()->segment(2) === null && array_get($skinConfig, 'titleStyle', 'titleWithCount') === 'titleWithCount')
+                        @if (array_get($skinConfig, 'titleStyle', 'titleWithCount') === 'titleWithCount')
                             <span class="xe-list-board-header__post-count">({{ number_format($paginate->total()) }})</span>
                         @endif
                     @endif
