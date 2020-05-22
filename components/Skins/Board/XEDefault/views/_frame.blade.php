fab7523f5b57cc97c5e1953571cca6c20ec958aa
mjae404 <mjae404@gmail.com>
2020-05-22 17:56:59 +0900

    new board 상단 공통 영역 위치 수정

--- components/Skins/Board/XEDefault/views/_frame.blade.php
+++ components/Skins/Board/XEDefault/views/_frame.blade.php
@@ -9,51 +9,51 @@
 {{ expose_trans('board::selectBoard') }}
 {{ expose_trans('board::msgDeleteConfirm') }}
 
-<section class="xe-list-board">
-    <div class="xe-list-board-header">
-        @if (request()->segment(2) === null)
-            <div class="xe-list-board-header__title-content">
-                <div class="xe-list-board-header__title-box">
-                    @if (in_array(array_get($skinConfig, 'titleStyle', 'titleWithCount'), ['titleWithCount', 'title']) === true)
-                        <h2 class="xe-list-board-header__title">
-                            <a href="{{ $urlHandler->get('index') }}">
-                                @if (xe_trans($config->get('boardName', '')) !== '')
-                                    {{ xe_trans($config->get('boardName')) }}
-                                @else
-                                    {{ xe_trans(current_menu()['title']) }}
-                                @endif
-                            </a>
-                        </h2>
-                        @if (array_get($skinConfig, 'titleStyle', 'titleWithCount') === 'titleWithCount')
-                            <span class="xe-list-board-header__post-count">({{ number_format($paginate->total()) }})</span>
+        
+<section class="xf-board">
+    @if ($config->get('topCommonContentOnlyList') === false || request()->segment(2) === '')
+        <div class="xe-list-board-header__text">
+            {!! xe_trans($config->get('topCommonContent', '')) !!}
+        </div>
+    @endif
+    <section class="xe-list-board">
+        <div class="xe-list-board-header">
+            @if (request()->segment(2) === null)
+                <div class="xe-list-board-header__title-content">
+                    <div class="xe-list-board-header__title-box">
+                        @if (in_array(array_get($skinConfig, 'titleStyle', 'titleWithCount'), ['titleWithCount', 'title']) === true)
+                            <h2 class="xe-list-board-header__title">
+                                <a href="{{ $urlHandler->get('index') }}">
+                                    @if (xe_trans($config->get('boardName', '')) !== '')
+                                        {{ xe_trans($config->get('boardName')) }}
+                                    @else
+                                        {{ xe_trans(current_menu()['title']) }}
+                                    @endif
+                                </a>
+                            </h2>
+                            @if (array_get($skinConfig, 'titleStyle', 'titleWithCount') === 'titleWithCount')
+                                <span class="xe-list-board-header__post-count">({{ number_format($paginate->total()) }})</span>
+                            @endif
+                        @endif
+                    </div>
+                    @if (array_get($skinConfig, 'visibleIndexMobileWriteButton', 'on') === 'on')
+                        @if (array_get($skinConfig, 'visibleIndexWriteButton', 'show') === 'show')
+                            <div class="xe-list-board-header__write-button">
+                                <a href="{{ $urlHandler->get('create') }}"><img src="{{ url('plugins/board/assets/img/pencil.svg') }}" alt="모바일 글쓰기 이미지"></a>
+                            </div>
+                        @elseif (request()->segment(2) === null && array_get($skinConfig, 'visibleIndexWriteButton', 'show') === 'permission' && $isWritable === true)
+                            <div class="xe-list-board-header__write-button">
+                                <a href="{{ $urlHandler->get('create') }}"><img src="{{ url('plugins/board/assets/img/pencil.svg') }}" alt="모바일 글쓰기 이미지"></a>
+                            </div>
                         @endif
                     @endif
                 </div>
-                @if (array_get($skinConfig, 'visibleIndexMobileWriteButton', 'on') === 'on')
-                    @if (array_get($skinConfig, 'visibleIndexWriteButton', 'show') === 'show')
-                        <div class="xe-list-board-header__write-button">
-                            <a href="{{ $urlHandler->get('create') }}"><img src="{{ url('plugins/board/assets/img/pencil.svg') }}" alt="모바일 글쓰기 이미지"></a>
-                        </div>
-                    @elseif (request()->segment(2) === null && array_get($skinConfig, 'visibleIndexWriteButton', 'show') === 'permission' && $isWritable === true)
-                        <div class="xe-list-board-header__write-button">
-                            <a href="{{ $urlHandler->get('create') }}"><img src="{{ url('plugins/board/assets/img/pencil.svg') }}" alt="모바일 글쓰기 이미지"></a>
-                        </div>
-                    @endif
-                @endif
-            </div>
-        @endif
-        
-        @if ($config->get('topCommonContentOnlyList') === false || request()->segment(2) === '')
-            <div class="xe-list-board-header__text">
-                {!! xe_trans($config->get('topCommonContent', '')) !!}
-            </div>
-        @endif
-    </div>
-
-    @section('content')
-        {!! isset($content) ? $content : '' !!}
-    @show
-    
+            @endif
+        </div>
+        @section('content')
+            {!! isset($content) ? $content : '' !!}
+        @show
+    </section>
     @if ($config->get('bottomCommonContentOnlyList') === false || request()->segment(2) === '')
         <div class="xe-list-board-footer__text">
             {!! xe_trans($config->get('bottomCommonContent', '')) !!}
