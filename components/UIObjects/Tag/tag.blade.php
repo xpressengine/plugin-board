a770cb5c13213cbb37b4f4b692d63eb3b28f0725
mjae404 <mjae404@gmail.com>
2020-10-06 16:50:37 +0900

    태그 입력 높이 수정

--- components/UIObjects/Tag/tag.blade.php
+++ components/UIObjects/Tag/tag.blade.php
@@ -3,43 +3,45 @@
 @endif
 
 <div id="{{ $id }}" class="{{ $class }}" data-tags="{{ $strTags }}" data-url="{{$url}}">
-    <vue-tags-input v-model="tag" :tags="tags" @tags-changed="update" :autocomplete-items="autocompleteItems" placeholder="{{ $placeholder }}"></vue-tags-input>
+    <vue-tags-input v-model="tag" :tags="tags" @tags-changed="update" :autocomplete-items="autocompleteItems"
+                    placeholder="{{ $placeholder }}"></vue-tags-input>
 </div>
 
 <style>
-  .xe-select-label {
-    height: 60px;
-  }
+    .xe-select-label {
+        height: auto;
+    }
 
-  .__xe-board-tag .vue-tags-input .ti-input {
-    max-width: 100%;
-  }
+    .__xe-board-tag .vue-tags-input .ti-input {
+        max-width: 100%;
+    }
 
-  .ti-input[data-v-61d92e31] {
-    border: none !important;
-    height: 55px;
-      align-items: center;
-  }
+    .ti-input[data-v-61d92e31] {
+        border: none !important;
+        min-height: 55px;
+        height: auto;
+        align-items: center;
+    }
 
-  .vue-tags-input[data-v-61d92e31] {
-    max-width: none;
-    line-height: 60px;
-    border-bottom: 1px solid #e0e0e0;
-  }
+    .vue-tags-input[data-v-61d92e31] {
+        max-width: none;
+        line-height: 60px;
+        border-bottom: 1px solid #e0e0e0;
+    }
 
-  .ti-tag.ti-invalid[data-v-61d92e31], .ti-tag.ti-tag.ti-deletion-mark[data-v-61d92e31] {
-    background-color: rgba(0, 0, 0, 0.8);
-    border-radius: 3px;
-      height: 25px !important;
-  }
+    .ti-tag.ti-invalid[data-v-61d92e31], .ti-tag.ti-tag.ti-deletion-mark[data-v-61d92e31] {
+        background-color: rgba(0, 0, 0, 0.8);
+        border-radius: 3px;
+        height: 25px !important;
+    }
 
-  .ti-new-tag-input-wrapper input[data-v-61d92e31]::placeholder {
-    font-size: 14px;
-    line-height: 25px;
-    letter-spacing: -0.2px;
-  }
+    .ti-new-tag-input-wrapper input[data-v-61d92e31]::placeholder {
+        font-size: 14px;
+        line-height: 25px;
+        letter-spacing: -0.2px;
+    }
 
-  .ti-new-tag-input-wrapper input[data-v-61d92e31] {
-    font-size: 14px;
-  }
+    .ti-new-tag-input-wrapper input[data-v-61d92e31] {
+        font-size: 14px;
+    }
 </style>
