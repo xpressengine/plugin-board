21b25553c03075c4f188f640a5e330c8b150fe32
xharpenParksuhyeon <parksu@xharpen.com>
2021-08-05 09:47:02 +0000

    #299 사용하지 않는 사용자 정의 항목은 관리자 > 회원 페이지 에서 값만 출력 (disalbed), '설정 > 가입 설정 > 사용자 정의 항목' 으로 이동하는 링크가 출력되도록 수정

--- components/DynamicFields/Category/Skins/DesignSelect/views/create.blade.php
+++ components/DynamicFields/Category/Skins/DesignSelect/views/create.blade.php
@@ -1,6 +1,12 @@
+@if (!$config->get('use', false))
+    @include('dynamicField.userActivateLink')
+@endif
+
+
 {!! uio('uiobject/board@select', [
     'name' => $key['item_id'],
     'label' => xe_trans($config->get('label')),
     'value' => Request::old($key['item_id']),
     'items' => $data['selectItems'],
+    'disabled' => !$config->get('use', false)
 ]) !!}
--- components/DynamicFields/Category/Skins/DesignSelect/views/edit.blade.php
+++ components/DynamicFields/Category/Skins/DesignSelect/views/edit.blade.php
@@ -1,6 +1,11 @@
+@if (!$config->get('use', false))
+    @include('dynamicField.userActivateLink')
+@endif
+
 {!! uio('uiobject/board@select', [
     'name' => $key['item_id'],
     'label' => xe_trans($config->get('label')),
     'value' => $data['item_id'] != null ? $data['item_id'] : '',
     'items' => $data['selectItems'],
+    'disabled' => !$config->get('use', false)
 ]) !!}
