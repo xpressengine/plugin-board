4c7d8b0b6ea7555ac99998bae35ce18958ca7bb5
xharpenParksuhyeon <parksu@xharpen.com>
2021-08-17 02:10:53 +0000

    abstract config handler 추가

---
+++ src/AbstractConfigHandler.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Xpressengine\Plugins\Board;
+
+use Xpressengine\Config\{ConfigEntity, ConfigManager};
+
+abstract class AbstractConfigHandler
+{
+    /**
+     * config package name
+     * 다른 모듈과 충돌을 피하기 위해 설정 이름을 모듈 이름으로 선언
+     */
+    const CONFIG_NAME = 'module/board@board';
+
+    /** @var ConfigManager */
+    protected $configManager;
+
+    /** @var array */
+    protected $defaultConfig = [];
+
+    public function __construct(ConfigManager $configManager) {
+        $this->configManager = $configManager;
+    }
+
+    public function getDefault(): ConfigEntity
+    {
+        $parent = $this->configManager->get(static::CONFIG_NAME);
+
+        if (! ($parent instanceof ConfigEntity)) {
+            $parent = $this->configManager->add(static::CONFIG_NAME, $this->defaultConfig);
+        }
+
+        return $parent;
+    }
+
+    public function getDefaultKeys(): array
+    {
+        return array_keys($this->defaultConfig);
+    }
+
+    public function existsDefault(): bool
+    {
+        $parent = $this->configManager->get(static::CONFIG_NAME);
+        return $parent instanceof ConfigEntity;
+    }
+
+    public function addDefault(array $args): ConfigEntity
+    {
+        return $this->configManager->add(static::CONFIG_NAME, $args);
+    }
+
+    public function putDefault(array $args): ConfigEntity
+    {
+        return $this->configManager->put(static::CONFIG_NAME, $args);
+    }
+
+    public function add(array $params): ConfigEntity
+    {
+        return $this->configManager->add($this->name($params['boardId']), $params);
+    }
+
+    public function put(array $params): ConfigEntity
+    {
+        return $this->configManager->put($this->name($params['boardId']), $params);
+    }
+
+    public function modify(ConfigEntity $config): ConfigEntity
+    {
+        return $this->configManager->modify($config);
+    }
+
+    public function remove(ConfigEntity $config)
+    {
+        $this->configManager->remove($config);
+    }
+
+    public function gets(): array
+    {
+        $parent = $this->configManager->get(static::CONFIG_NAME);
+        return $parent === null ? [] : $this->configManager->children($parent);
+    }
+
+    public function get(string $boardId): ConfigEntity
+    {
+        $name = $this->name($boardId);
+        $config = $this->configManager->get($name);
+
+        if (! ($config instanceof ConfigEntity)) {
+            if (!$this->existsDefault()) {
+                $this->getDefault();
+            }
+
+            return $this->configManager->add($name, []);
+        }
+
+        return $config;
+    }
+
+    protected function name(string $boardId): string
+    {
+        return sprintf('%s.%s', static::CONFIG_NAME, $boardId);
+    }
+}
--- src/ConfigHandler.php
+++ src/ConfigHandler.php
@@ -28,18 +28,13 @@ use Xpressengine\Document\ConfigHandler as DocumentConfigHandler;
  * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
  * @link        https://xpressengine.io
  */
-class ConfigHandler
+class ConfigHandler extends AbstractConfigHandler
 {
     /**
      * config package name
      * 다른 모듈과 충돌을 피하기 위해 설정 이름을 모듈 이름으로 선언
      */
     const CONFIG_NAME = 'module/board@board';
-
-    /**
-     * @var ConfigManager
-     */
-    protected $configManager;
 
     /**
      * @var DynamicFieldConfigHandler
@@ -120,133 +115,9 @@ class ConfigHandler
         DynamicFieldConfigHandler $dynamicField,
         DocumentConfigHandler $document
     ) {
-        $this->configManager = $configManager;
+        parent::__construct($configManager);
         $this->dynamicField = $dynamicField;
         $this->document = $document;
-    }
-
-    /**
-     * 기본 게시판 설정 반환. 설정이 없을 경우 등록 후 반환
-     *
-     * @return ConfigEntity
-     */
-    public function getDefault()
-    {
-        $parent = $this->configManager->get(static::CONFIG_NAME);
-
-        if ($parent == null) {
-            $default = $this->defaultConfig;
-            $parent = $this->configManager->add(static::CONFIG_NAME, $default);
-        }
-
-        return $parent;
-    }
-
-    /**
-     * 기본 게시판 설정 등록
-     *
-     * @param array $args config arguments
-     * @return ConfigEntity
-     */
-    public function addDefault(array $args)
-    {
-        return $this->configManager->add(static::CONFIG_NAME, $args);
-    }
-
-    /**
-     * 기본 게시판 설정 수정
-     *
-     * @param array $args config arguments
-     * @return ConfigEntity
-     */
-    public function putDefault(array $args)
-    {
-        return $this->configManager->put(static::CONFIG_NAME, $args);
-    }
-
-    /**
-     * 게시판 인스턴스 설정 이름 반환
-     *
-     * @param string $boardId board id
-     * @return string
-     */
-    private function name($boardId)
-    {
-        return sprintf('%s.%s', static::CONFIG_NAME, $boardId);
-    }
-
-    /**
-     * add config
-     *
-     * @param array $params parameters
-     * @return ConfigEntity
-     * @throws \Xpressengine\Config\Exceptions\InvalidArgumentException
-     */
-    public function add(array $params)
-    {
-        return $this->configManager->add($this->name($params['boardId']), $params);
-    }
-
-    /**
-     * put config
-     *
-     * @param array $params parameters
-     * @return ConfigEntity
-     * @throws \Xpressengine\Config\Exceptions\InvalidArgumentException
-     */
-    public function put(array $params)
-    {
-        return $this->configManager->put($this->name($params['boardId']), $params);
-    }
-
-    /**
-     * modify config
-     *
-     * @param ConfigEntity $config config entity
-     * @return ConfigEntity
-     */
-    public function modify(ConfigEntity $config)
-    {
-        return $this->configManager->modify($config);
-    }
-
-    /**
-     * remove config
-     *
-     * @param ConfigEntity $config board config entity
-     * @return void
-     */
-    public function remove(ConfigEntity $config)
-    {
-        $this->configManager->remove($config);
-    }
-
-    /**
-     * get all board configs
-     *
-     * @return array
-     */
-    public function gets()
-    {
-        $parent = $this->configManager->get(static::CONFIG_NAME);
-        if ($parent === null) {
-            return [];
-        }
-
-        $configs = $this->configManager->children($parent);
-        return $configs;
-    }
-
-    /**
-     * get board config
-     *
-     * @param string $boardId board id
-     * @return ConfigEntity
-     */
-    public function get($boardId)
-    {
-        $config = $this->configManager->get($this->name($boardId));
-        return $config;
     }
 
     /**
@@ -258,10 +129,7 @@ class ConfigHandler
     public function getDynamicFields(ConfigEntity $config)
     {
         $configs = $this->dynamicField->gets($config->get('documentGroup'));
-        if (count($configs) == 0) {
-            return [];
-        }
-        return $configs;
+        return count($configs) == 0 ? [] : $configs;
     }
 
     /**
@@ -270,7 +138,7 @@ class ConfigHandler
      * @param string $boardId board id
      * @return ConfigEntity
      */
-    public function getDocument($boardId)
+    public function getDocument(string $boardId): ConfigEntity
     {
         return $this->document->get($boardId);
     }
